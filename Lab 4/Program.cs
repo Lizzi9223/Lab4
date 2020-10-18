73962ed381a67d3af7c1fb6ab1e97d906be210e6
Lizzi <lizaveta9223@gmail.com>
2020-10-18 16:40:09 +0300

    1st task

---
+++ Lab 4/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vector vect1 = new Vector(123,-56);
+            Vector vect2 = new Vector(2, 4);
+            Vector empt_vect = new Vector();
+
+            Console.Write("vect1\t");
+            vect1.show();
+            Console.Write("vect2\t");
+            vect2.show();
+            Console.Write("empt_vect\t");
+            empt_vect.show();
+
+            Vector sum = vect1 + vect2;
+            Console.Write("sum = vect1 + vect2\t");
+            sum.show();
+            bool compare = vect1 > vect2;
+            if (compare) Console.WriteLine("vect1 > vect2 ");
+            else Console.WriteLine("vect1 > vect2 ");
+            vect1 = (vect1 == vect2);
+            Console.Write("\nvect2 скопирован в vect1\t");
+            vect1.show();
+            if (empt_vect) Console.WriteLine("Вектор empt_vect НЕ пустой");
+            else Console.WriteLine("Вектор empt_vect пустой");
+        }
+    }
+}
---
+++ Lab 4/Vector.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_4
+{
+    class Vector
+    {
+        public int x;
+        public int y;
+
+        public Vector (int value1, int value2)
+        {
+            x = value1;
+            y = value2;
+        }
+        public Vector()
+        {
+           
+        }
+        public double length()
+        {
+            return Math.Sqrt(x * x + y * y);
+        }
+        public void show()
+        {
+            Console.WriteLine($"Координаты вектора : x = {x}, y = {y}; Длина вектора : {this.length()}\n");
+        }
+
+        public static Vector operator + (Vector one, Vector two)
+        {
+            return new Vector(one.x + two.x, one.y + two.y);
+        }
+
+        public static bool operator >(Vector one, Vector two)
+        {
+            return one.length() > two.length();
+        }
+        public static bool operator <(Vector one, Vector two)
+        {
+            return one.length() < two.length();
+        }
+        //копирование вектора two в вектор one:
+        public static Vector operator == (Vector one, Vector two)
+        {
+            one.x = two.x;
+            one.y = two.y;
+            return one;
+        }
+        //копирование вектора two в вектор one:
+        public static Vector operator !=(Vector one, Vector two)
+        {
+            one.x = two.x;
+            one.y = two.y;
+            return one;
+        }
+        //проверка пустой ли вектор
+        public static bool operator true(Vector one)
+        {
+            int? check = one.x;
+            if (check.HasValue) return true;
+            else return false;
+        }
+        //проверка вектора на наличие значения
+        public static bool operator false(Vector one)
+        {
+            int? check = one.x;
+            if (check.HasValue) return true;
+            else return false;
+        }
+    }
+}
